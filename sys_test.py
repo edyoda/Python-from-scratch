9ff92e58c4d4788080de42e403d7776d0142dd1f
kunal665 <kunaldk665@gmail.com>
2019-04-21 15:30:39 +0530

    Add files via upload

---
+++ sys_test.py
@@ -0,0 +1,47 @@
+# import sys
+# # fp = open("sys_err.txt","w")
+# # sys.stderr = fp 
+# # prin, 't(10/0)
+
+# print(sys.argv)
+# num1 = int(sys.argv[1])
+# num2 = int(sys.argv[2])
+# print(num1 + num2)
+# 
+# import os 
+# print(os.getcwd())
+# print(os.listdir()[3])
+
+# file_name = os.listdir()[3]
+# os.system("python {}".format(file_name))
+
+
+# os.mkdir("new_dir")
+# print(os.listdir())
+
+# os.chdir("new_dir")
+# print(os.getcwd())
+# import pdb 
+
+# def linear_search(l,key):
+# 	for value in l :
+# 		if key == value:
+# 			return True
+# 	else:
+# 		return False
+
+# pdb.set_trace()
+# l = [10,20,30,40,50]
+# key = 400 
+# print(linear_search(l,key))
+
+
+# n = > next line 
+# list => to get list of lines to be executed 
+# s => step => to get inside of func 
+# c => continue => come out of pdb and execute the code 
+
+
+
+
+
